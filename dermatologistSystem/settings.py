74c425f724df5d9bd6bcfb90e2dc5bd6bd1694d9
RamsonAI <callmekali18@gmail.com>
2024-10-13 12:46:44 +0300

    minor changes

--- dermatologistSystem/settings.py
+++ dermatologistSystem/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'dermatologistSystem.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'aidermatologist',
+        'PORT':'5432',
+        'USER':'postgres',
+        'PASSWORD': 'callmekali',
+        'HOST':'localhost',
     }
 }
 
